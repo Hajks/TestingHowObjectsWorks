6a8608d81bca54e87599e07b088122b537ae2b11
Hajk <tunczorx@gmail.com>
2018-03-12 15:41:44 +0100

    Created some objects and watched how they look like.

---
+++ TestingHowObjectsWorks/Clown.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingHowObjectsWorks
+{
+    public class Clown
+    {
+        //We declare two public variables and creating method which let our clowns describe themself
+        public string Name;
+        public int Height;
+
+        public void TalkAboutYourSelf()
+        {
+            MessageBox.Show("Nazywam się " + Name + " i mam " + Height + " centymetrów wzrostu", Name + " mówi.."); //Message containing our variables like Name and Heigh. 
+        }
+
+    }
+
+}
--- TestingHowObjectsWorks/Form1.cs
+++ TestingHowObjectsWorks/Form1.cs
@@ -16,5 +16,25 @@ namespace TestingHowObjectsWorks
         {
             InitializeComponent();
         }
+        // After clicking button, our 3 new objects will display their variables
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                Clown oneClown = new Clown();
+                oneClown.Name = "Bartek";
+                oneClown.Height = 150 ;
+                oneClown.TalkAboutYourSelf(); //We use our new object clown with method created in Clown.cs in this way we can save time and make the code more readable
+
+                Clown secondClown = new Clown();
+                secondClown.Name = "Jasiek";
+                secondClown.Height = 178;
+                secondClown.TalkAboutYourSelf();
+
+                Clown thirdClown = new Clown();
+                thirdClown.Name = "Dawid";
+                thirdClown.Height = 193 - oneClown.Height; // We can use data they store and use them like normal variables. For example clown one is storing .Height = 150. So 193 - 150 should equal 43
+                thirdClown.TalkAboutYourSelf();
+            
+        }
     }
 }
